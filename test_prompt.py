8712629fbcff6686ef81ca29714e0320f6925a90
natukin1978 <natukin1978@hotmail.com>
2024-06-16 12:05:29 +0900

    Change to pass preconditions first

--- test_prompt.py
+++ test_prompt.py
@@ -14,7 +14,9 @@ config = readConfig()
 genai.configure(api_key=config["google"]["geminiApiKey"])
 genaiModel = genai.GenerativeModel(config["google"]["modelName"])
 genaiChat = genaiModel.start_chat(history=[])
+responseAI = genaiChat.send_message(config["basePrompt"])
+print(responseAI.text)
 
 for question in questions:
-    responseAI = genaiChat.send_message(question + "\n" + config["basePrompt"])
+    responseAI = genaiChat.send_message(question)
     print(responseAI.text)
