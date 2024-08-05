d251fdf9f7b02fc9b05a7725bf05c3ba3f291651
natukin1978 <natukin1978@hotmail.com>
2024-08-05 23:31:17 +0900

    Read and reflect AI voice from the settings file

--- talk_voice.py
+++ talk_voice.py
@@ -1,14 +1,37 @@
 import aiohttp
 
+from typing import Any
+import global_value as g
+
+
+async def _request_voice_base(suffix_param: str) -> aiohttp.ClientResponse:
+    configAS = g.config["assistantSeika"]
+    url = f"http://{configAS['name']}:{configAS['port']}/{suffix_param}"
+    auth = aiohttp.BasicAuth(login=configAS["login"], password=configAS["password"])
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, auth=auth) as response:
+            return response
+
+
+async def set_voice_effect(param: str, value: Any) -> None:
+    try:
+        configAS = g.config["assistantSeika"]
+        cid = configAS["cid"]
+        if not cid:
+            return
+        suffix_param = f"EFFECT/{cid}/{param}/{value}"
+        await _request_voice_base(suffix_param)
+    except Exception as e:
+        print(e)
+
 
 async def talk_voice(text: str) -> None:
     try:
-        url = "http://localhost:7180/PLAYASYNC2/90011/" + text
-        auth = aiohttp.BasicAuth(
-            login="SeikaServerUser", password="SeikaServerPassword"
-        )
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, auth=auth) as response:
-                print(await response.text())
+        configAS = g.config["assistantSeika"]
+        cid = configAS["cid"]
+        if not cid:
+            return
+        suffix_param = f"PLAYASYNC2/{cid}/{text}"
+        await _request_voice_base(suffix_param)
     except Exception as e:
-        pass
+        print(e)
