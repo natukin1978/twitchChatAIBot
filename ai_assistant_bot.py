987ba99c32b24f1a142ed10b8e6509cf8e21654e
natukin1978 <natukin1978@hotmail.com>
2024-09-15 17:05:19 +0900

    I tried using websocket-client (doesn't work)

--- ai_assistant_bot.py
+++ ai_assistant_bot.py
@@ -1,9 +1,10 @@
 import asyncio
 import json
 import re
+import time
 
 import twitchio
-import websockets
+import websocket
 
 import global_value as g
 from config_helper import readConfig
@@ -29,46 +30,56 @@ g.talk_buffers = ""
 
 
 async def main():
-    async def connect_and_receive(uri):
-        async with websockets.connect(uri) as websocket:
-            while True:
-                try:
-                    message = await websocket.recv()
-                    try:
-                        data = json.loads(message)
-                        # JSONとして処理する
-                        g.talker_name = data["talkerName"]
-                    except json.JSONDecodeError:
-                        # プレーンテキストとして処理する
-                        message = message.strip()
-                        talk_buffers_len = len(g.talk_buffers)
-                        answerLevel = 2
-                        if (
-                            talk_buffers_len > 1000
-                            or ("教え" in message)
-                            or ("調べ" in message)
-                            or is_hit(answerLevel)
-                        ):
-                            json_data = GenAI.create_message_json()
-                            json_data["id"] = g.config["twitch"]["loginChannel"]
-                            json_data["displayName"] = g.talkerName
-                            json_data["content"] = message.strip()
-                            update_message_json(json_data)
-                            response_text = genai.send_message_by_json_with_buf(
-                                json_data
-                            )
-                            if response_text:
-                                await client.get_channel(
-                                    g.config["twitch"]["loginChannel"]
-                                ).send(response_text)
-                        else:
-                            if talk_buffers_len > 0:
-                                g.talk_buffers += " "
-                            g.talk_buffers += message
+    def on_message(ws, message):
+        try:
+            data = json.loads(message)
+            # JSONとして処理する
+            g.talker_name = data["talkerName"]
+        except json.JSONDecodeError:
+            # プレーンテキストとして処理する
+            message = message.strip()
+            talk_buffers_len = len(g.talk_buffers)
+            answerLevel = 2
+            if (
+                talk_buffers_len > 1000
+                or ("教え" in message)
+                or ("調べ" in message)
+                or is_hit(answerLevel)
+            ):
+                json_data = GenAI.create_message_json()
+                json_data["id"] = g.config["twitch"]["loginChannel"]
+                json_data["displayName"] = g.talker_name
+                json_data["content"] = message.strip()
+                update_message_json(json_data)
+                response_text = genai.send_message_by_json_with_buf(json_data)
+                if response_text:
+                    asyncio.ensure_future(channel_send(response_text))
+            else:
+                if talk_buffers_len > 0:
+                    g.talk_buffers += " "
+                g.talk_buffers += message
 
-                except websockets.exceptions.ConnectionClosed:
-                    print("WebSocket connection closed")
-                    break
+    def on_error(ws, error):
+        print(error)
+
+    def on_close(ws, close_status_code, close_msg):
+        if ws:
+            print("### closed ###")
+            time.sleep(5)
+        ws = websocket.WebSocketApp(
+            g.websocket_uri,
+            on_message=on_message,
+            on_error=on_error,
+            on_close=on_close,
+            on_open=on_open,
+        )
+        ws.run_forever()
+
+    def on_open(ws):
+        print("Opened connection")
+
+    async def channel_send(response_text):
+        await client.get_channel(g.config["twitch"]["loginChannel"]).send(response_text)
 
     genai = GenAI()
     print("base_prompt:")
@@ -84,9 +95,9 @@ async def main():
     await bot.connect()
 
     conf_rs = g.config["recvServer"]
-    websocket_uri = f"ws://{conf_rs['name']}:{conf_rs['port']}/textonly"
-    websocket_task = asyncio.create_task(connect_and_receive(websocket_uri))
-    await websocket_task
+    g.websocket_uri = f"ws://{conf_rs['name']}:{conf_rs['port']}/textonly"
+
+    on_close(None, None, None)
 
 
 if __name__ == "__main__":
